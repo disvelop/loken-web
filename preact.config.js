2acd1d05f5df364c22a7ccca4a6d567672d2bc84
Thomas McNiven <hello@vevix.net>
2018-03-18 19:40:44 +1000

    Fix build by including runtime babel plugin

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,13 @@
 const preactCliLodash = require('preact-cli-lodash');
  
-export default function (config) {
+export default function (config, env, helpers) {
   preactCliLodash(config);
+  
+  let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
+  rule.options.plugins.push('transform-regenerator');
+  rule.options.plugins.push(["transform-runtime", {
+    "helpers": false,
+    "polyfill": false,
+    "regenerator": true
+  }]);
 }
